3557389ba886cd236becd0650ce3006f1497695c
eternal17 <sarmadkhatri15@.com>
2021-09-07 11:48:50 +0100

    using cut to omit last three characters

--- lookagain.sh
+++ lookagain.sh
@@ -1,8 +1,1 @@
-# Create a file lookagain.sh, which will look, 
-#from the current directory and its sub-folders for:
-#all the files ending with .sh.
-
-#That command will only show the name of the files without the .sh. 
-#The files will be in descending order (as shown in the below example).
-
-find . -name '*.sh' -prune -o -print
+find . -name '*.sh'| rev | cut -c 4- | rev

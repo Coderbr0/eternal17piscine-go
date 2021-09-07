461236c0f6bb2effdf2dfb8bffd59d418cc18bb4
eternal17 <sarmadkhatri15@.com>
2021-09-07 12:11:36 +0100

    using cut and rev

--- lookagain.sh
+++ lookagain.sh
@@ -1,1 +1,3 @@
-find . -name '*.sh'| rev | cut -c 4- | rev
+# reverse the output, cut the first 3 and show the rest, reverse, cut the first 2
+
+find . -name '*.sh'| rev | cut -c 4- | rev | cut -c 3-

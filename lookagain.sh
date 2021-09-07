c7f537e3af1916bddf8d5edc1cb0c81bdd3f39c3
eternal17 <sarmadkhatri15@.com>
2021-09-07 12:38:06 +0100

    not showing path using printf

--- lookagain.sh
+++ lookagain.sh
@@ -1,3 +1,4 @@
 # reverse the output, cut the first 3 and show the rest, reverse, cut the first 2
 
-find . -name '*.sh'| rev | cut -c 4- | rev | cut -c 3-
+find -name '*.sh' -printf '%f\n'| rev | cut -c 4- | rev
+

59334ed7c58892c6841456c5e18dcd11425c8009
eternal17 <sarmadkhatri15@gmail.com>
2021-09-08 11:01:15 +0100

    added the digit 0

--- printdigits/main.go
+++ printdigits/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var digits string = "123456789"
+	var digits string = "0123456789"
 	for i := 0; i <= len(digits)-1; i++ {
 		z01.PrintRune(rune(digits[i]))
 	}

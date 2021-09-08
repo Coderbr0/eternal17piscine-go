7be59d788ab50c0dacadb154711daf0c8113899c
eternal17 <sarmadkhatri15@gmail.com>
2021-09-08 16:14:49 +0100

    changed condition

--- printcomb.go
+++ printcomb.go
@@ -11,7 +11,7 @@ import "github.com/01-edu/z01"
 func PrintComb() {
 	for i := 48; i < 56; i++ {
 		for j := 49; j < 57; j++ {
-			for k := 51; k < 58; k++ {
+			for k := 50; k < 58; k++ {
 				z01.PrintRune(rune(i))
 				z01.PrintRune(rune(j))
 				z01.PrintRune(rune(k))

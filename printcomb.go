095ee040fc09b96201c428e970c7afd088ef6c8a
eternal17 <sarmadkhatri15@gmail.com>
2021-09-08 19:01:12 +0100

    added new conditionals

--- printcomb.go
+++ printcomb.go
@@ -4,8 +4,8 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := 48; i < 56; i++ {
-		for j := 49; j < 57; j++ {
-			for k := 50; k < 58; k++ {
+		for j := 49; j < 57 && i < j; j++ {
+			for k := 50; k < 58 && i < j && j < k; k++ {
 				z01.PrintRune(rune(i))
 				z01.PrintRune(rune(j))
 				z01.PrintRune(rune(k))

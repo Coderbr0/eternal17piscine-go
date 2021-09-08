bc616dfa7d3c0e19638ff41415b4fe500c091bf9
eternal17 <sarmadkhatri15@gmail.com>
2021-09-08 17:58:02 +0100

    reverted back

--- printcomb.go
+++ printcomb.go
@@ -4,14 +4,21 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := 48; i < 56; i++ {
-		z01.PrintRune(rune(i))
 		for j := 49; j < 57; j++ {
-			z01.PrintRune(rune(j))
 			for k := 50; k < 58; k++ {
+				z01.PrintRune(rune(i))
+				z01.PrintRune(rune(j))
 				z01.PrintRune(rune(k))
 				z01.PrintRune(rune(44))
 				z01.PrintRune(rune(32))
 			}
 		}
 	}
+}
+package main
+
+import "piscine"
+
+func main() {
+	piscine.PrintComb()
 }

b56657c21c89cccfe8f08b2f7e7366a21ccd503e
eternal17 <sarmadkhatri15@gmail.com>
2021-09-08 20:25:17 +0100

    added if else statement

--- printcomb.go
+++ printcomb.go
@@ -4,15 +4,23 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := 48; i < 56; i++ {
-		for j := 49; j < 57; j++ {
-			for k := 50; k < 58; k++ {
-				z01.PrintRune(rune(i))
-				z01.PrintRune(rune(j))
-				z01.PrintRune(rune(k))
-				z01.PrintRune(rune(44))
-				z01.PrintRune(rune(32))
+		for j := i + 1; j < 57; j++ {
+			for k := j + 1; k < 58; k++ {
+				if i == 55 && j == 56 && k == 57 {
+					z01.PrintRune(rune(i))
+					z01.PrintRune(rune(j))
+					z01.PrintRune(rune(k))
+					z01.PrintRune(rune(44))
+					z01.PrintRune(rune(32))
+					z01.PrintRune('\n')
+				} else {
+					z01.PrintRune(rune(i))
+					z01.PrintRune(rune(j))
+					z01.PrintRune(rune(k))
+					z01.PrintRune(rune(44))
+					z01.PrintRune(rune(32))
+				}
 			}
 		}
 	}
-	z01.PrintRune('\n')
 }

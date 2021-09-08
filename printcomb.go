a60172b4d173a736380c37bb33ab9b508011248a
eternal17 <sarmadkhatri15@gmail.com>
2021-09-08 17:49:25 +0100

    changed location of printrune

--- printcomb.go
+++ printcomb.go
@@ -2,18 +2,12 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-// Write a function that prints,
-// in ascending order and on a single line:
-// all unique combinations of three different digits so that,
-// the first digit is lower than the second, and the second is lower than the third.
-// These combinations are separated by a comma and a space.
-
 func PrintComb() {
 	for i := 48; i < 56; i++ {
+		z01.PrintRune(rune(i))
 		for j := 49; j < 57; j++ {
+			z01.PrintRune(rune(j))
 			for k := 50; k < 58; k++ {
-				z01.PrintRune(rune(i))
-				z01.PrintRune(rune(j))
 				z01.PrintRune(rune(k))
 				z01.PrintRune(rune(44))
 				z01.PrintRune(rune(32))

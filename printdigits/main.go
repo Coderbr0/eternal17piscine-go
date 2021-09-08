556ec59dfecbe7cebcd2ff915723db1be5c30288
eternal17 <sarmadkhatri15@gmail.com>
2021-09-08 10:52:04 +0100

    converting a string into a rune

--- printdigits/main.go
+++ printdigits/main.go
@@ -1,15 +1,16 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func main() {
-	for i := 0; i <= 9; i++ {
-		fmt.Println(i)
-		z01.PrintRune('\n')
+// Declare a variable string
+	var digits string = "123456789"
+// Loop through string until length -1 as length is returned as bytes
+	for i := 0; i <= len(digits) -1; i++ {
+// use printrune and convert the string digits into a rune
+	z01.PrintRune(rune(digits[i]))
 	}
+	//end the line
+	z01.PrintRune('\n')
 }
-// test
+

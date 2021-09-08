e18741efaf4c115275b9e12e5c0d2a0b62bd5f16
eternal17 <sarmadkhatri15@gmail.com>
2021-09-08 10:59:53 +0100

    sorted formatting

--- printdigits/main.go
+++ printdigits/main.go
@@ -3,14 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-// Declare a variable string
 	var digits string = "123456789"
-// Loop through string until length -1 as length is returned as bytes
-	for i := 0; i <= len(digits) -1; i++ {
-// use printrune and convert the string digits into a rune
-	z01.PrintRune(rune(digits[i]))
+	for i := 0; i <= len(digits)-1; i++ {
+		z01.PrintRune(rune(digits[i]))
 	}
-	//end the line
 	z01.PrintRune('\n')
 }
-

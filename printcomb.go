128032997ca112c29cc994b3e1a75d626cafafe6
eternal17 <sarmadkhatri15@gmail.com>
2021-09-08 16:01:34 +0100

    unique combinations

---
+++ printcomb.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+// Write a function that prints,
+// in ascending order and on a single line:
+// all unique combinations of three different digits so that,
+// the first digit is lower than the second, and the second is lower than the third.
+// These combinations are separated by a comma and a space.
+
+func PrintComb() {
+	for i := 48; i <= 55; i++ {
+		for j := 49; j <= 56; j++ {
+			for k := 50; k <= 57; k++ {
+				z01.PrintRune(rune(i))
+				z01.PrintRune(rune(j))
+				z01.PrintRune(rune(k))
+				z01.PrintRune(rune(44))
+				z01.PrintRune(rune(40))
+			}
+		}
+	}
+}

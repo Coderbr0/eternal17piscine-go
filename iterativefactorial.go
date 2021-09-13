06a359db53078364a7be9daef330b806700eb8d2
eternal17 <sarmadkhatri15@gmail.com>
2021-09-13 13:32:18 +0100

    added correct conditional

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,13 +2,11 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-
+	if nb < 0 && nb >= 20 {
+		return 0
+	}
 	for i := 1; i <= nb; i++ {
 		result = result * i
 	}
-	if nb > 0 && nb <= 20 {
-		return result
-	} else {
-		return 0
-	}
+	return result
 }

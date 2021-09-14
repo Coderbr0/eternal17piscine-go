28a9ea74d3d97cdfab42ef38c747544f133f2ad7
eternal17 <sarmadkhatri15@gmail.com>
2021-09-14 23:59:45 +0100

    changed conditional

--- index.go
+++ index.go
@@ -4,12 +4,14 @@ func Index(s string, toFind string) int {
 	if s == "" || toFind == "" {
 		return 0
 	}
+	firstIndex := -1
 	for i := range s {
 		if s[i] == toFind[0] {
+			firstIndex = i
 			tempString := s[i : i+len(toFind)]
 
 			if tempString == toFind {
-				return i
+				return firstIndex
 			}
 		}
 	}

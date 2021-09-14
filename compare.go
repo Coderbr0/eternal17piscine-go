7abc48e555e61fc8a43abe216d7ce68db2431374
eternal17 <sarmadkhatri15@gmail.com>
2021-09-14 17:25:56 +0100

    changed conditionals

--- compare.go
+++ compare.go
@@ -1,18 +1,34 @@
 package piscine
 
+// func Compare(a, b string) int {
+// 	x := []byte(a)
+// 	y := []byte(b)
+// 	for i := range x {
+// 		for j := range y {
+// 			if x[i] > y[j] || x[i] == y[j] && len(a) > len(b) {
+// 				return 1
+// 			} else if x[i] < y[j] || x[i] == y[j] && len(b) > len(a) {
+// 				return -1
+// 			} else {
+// 				return 0
+// 			}
+// 		}
+// 	}
+// 	return 100
+// }
+
 func Compare(a, b string) int {
 	x := []byte(a)
 	y := []byte(b)
-
-	for i := range a {
-		for j := range b {
-			if x[i] < y[j] {
+	if a == b {
+		return 0
+	}
+	for i := range x {
+		for j := range y {
+			if x[i] < y[j] || x[i+1] < y[j+1] {
 				return -1
-			} else if x[i] > y[j] {
+			} else {
 				return 1
-			} else if x[i] == y[j] && len(x) != len(y) {
-				x[i] = x[i+1]
-				y[j] = y[j+1]
 			}
 		}
 	}

380439234c6283c10b6b2f1a3d591542644c89e0
eternal17 <sarmadkhatri15@gmail.com>
2021-09-14 20:50:56 +0100

    added =

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(s string) int {
 	count := 0
 	for i := 0; i <= len(s)-1; i++ {
-		if ('a' < s[i] && s[i] < 'z') || ('A' < s[i] && s[i] < 'Z') {
+		if ('a' <= s[i] && s[i] <= 'z') || ('A' <= s[i] && s[i] <= 'Z') {
 			count++
 		}
 	}

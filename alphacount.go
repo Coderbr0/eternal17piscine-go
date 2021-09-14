41974282fbabfc7c48eba6257bcfeda24cd44f09
eternal17 <sarmadkhatri15@gmail.com>
2021-09-14 20:47:54 +0100

    alphabet character counter

---
+++ alphacount.go
@@ -0,0 +1,11 @@
+package piscine
+
+func AlphaCount(s string) int {
+	count := 0
+	for i := 0; i <= len(s)-1; i++ {
+		if ('a' < s[i] && s[i] < 'z') || ('A' < s[i] && s[i] < 'Z') {
+			count++
+		}
+	}
+	return count
+}

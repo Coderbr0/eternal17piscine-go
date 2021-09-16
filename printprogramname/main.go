8d15d7693556a1d290a64df135ae463e48be4606
eternal17 <sarmadkhatri15@gmail.com>
2021-09-16 12:45:40 +0100

    just print first character

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -11,8 +11,8 @@ func main() {
 	programName := arg[0]
 	nRune := []rune(programName)
 
-	for i := range nRune {
-		z01.PrintRune(nRune[i])
-	}
+	// for i := range nRune {
+	z01.PrintRune(nRune[0])
+	// }
 	// z01.PrintRune('\n')
 }

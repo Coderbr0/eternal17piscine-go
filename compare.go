12a9bb367493d32fae699ec4df7c146fe5ce27e9
eternal17 <sarmadkhatri15@gmail.com>
2021-09-16 16:42:53 +0100

    removed comments

--- compare.go
+++ compare.go
@@ -1,22 +1,5 @@
 package piscine
 
-// func Compare(a, b string) int {
-// 	x := []byte(a)
-// 	y := []byte(b)
-// 	for i := range x {
-// 		for j := range y {
-// 			if x[i] > y[j] || x[i] == y[j] && len(a) > len(b) {
-// 				return 1
-// 			} else if x[i] < y[j] || x[i] == y[j] && len(b) > len(a) {
-// 				return -1
-// 			} else {
-// 				return 0
-// 			}
-// 		}
-// 	}
-// 	return 100
-// }
-
 func Compare(a, b string) int {
 	if a == b {
 		return 0

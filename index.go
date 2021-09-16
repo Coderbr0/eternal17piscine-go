18b4721936832d9e0410175b80f488ae72dd127c
eternal17 <sarmadkhatri15@gmail.com>
2021-09-16 17:19:59 +0100

    changed structure of code, not using runes

--- index.go
+++ index.go
@@ -1,18 +1,18 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	sRune := []rune(s)
-	tfRune := []rune(toFind)
-	firstIndex := -1
-	for i := 0; i < len(sRune); i++ {
-		if sRune[i] == tfRune[0] {
-			firstIndex = i
-			tempString := sRune[i : i+len(tfRune)]
 
-			if string(tempString) == string(toFind) {
+	var firstIndex int
+
+	for i := 0; i <= len(s)-1; i++ {
+		if s[i] == toFind[0] {
+			firstIndex = i
+			temp := s[i : i+len(toFind)]
+			if temp == toFind {
 				return firstIndex
 			}
 		}
 	}
+
 	return -1
 }

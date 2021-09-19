76cbb048328912d5a754f2469f87a1017a4f50cd
eternal17 <sarmadkhatri15@gmail.com>
2021-09-19 20:14:40 +0100

    returning slice of strings, given string

---
+++ splitwhitespaces.go
@@ -0,0 +1,45 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+
+	// starts from 1 to account for the extra word after spaces are counted
+	wordCount := 1
+
+	// this word will reset to empty after encountering a seperator
+	word := ""
+
+	for _, ch := range s {
+		if WhiteSpace(ch) {
+			wordCount++
+		}
+	}
+
+	finalString := make([]string, wordCount)
+	stringIndex := 0
+
+	// adding words to final string after every seperator, and resetting word to empty.
+	for _, ch := range s {
+		if WhiteSpace(ch) && word != "" {
+			finalString[stringIndex] = word
+			stringIndex++
+			word = ""
+			// add the rest of the string to word
+		} else {
+			word += string(ch)
+		}
+
+		// for last word as there may be no seperators after word
+		x := len(finalString)
+		if word != "" {
+			finalString[x-1] = word
+		}
+	}
+	return finalString
+}
+
+//  checks if the char or rune is a seperator
+func WhiteSpace(a rune) bool {
+
+	return a == '\n' || a == ' ' || a == '\t'
+
+}

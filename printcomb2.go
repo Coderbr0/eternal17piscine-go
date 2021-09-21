993cfd8a8293c4aafa3508913327c4dc24dbd8d6
eternal17 <sarmadkhatri15@gmail.com>
2021-09-21 14:38:13 +0100

    all possible combinations of 2, 2 digit numbers

---
+++ printcomb2.go
@@ -0,0 +1,30 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for d1 := '0'; d1 <= '9'; d1++ {
+		for d2 := '0'; d2 <= '9'; d2++ {
+			for d3 := '0'; d3 <= '9'; d3++ {
+				for d4 := '0'; d4 <= '9'; d4++ {
+					if d1 == '9' && d2 == '9' && d3 == '9' && d4 == '9' {
+						z01.PrintRune(d1)
+						z01.PrintRune(d2)
+						z01.PrintRune(' ')
+						z01.PrintRune(d3)
+						z01.PrintRune(d4)
+						z01.PrintRune('\n')
+					} else {
+						z01.PrintRune(d1)
+						z01.PrintRune(d2)
+						z01.PrintRune(' ')
+						z01.PrintRune(d3)
+						z01.PrintRune(d4)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+}

dfb4520eb2aa6c36e7764210060b6856ca8ce672
eternal17 <sarmadkhatri15@gmail.com>
2021-09-21 16:20:44 +0100

    converting number to rune equivalent

---
+++ printnbr.go
@@ -0,0 +1,31 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func Convert(nb int) {
+	runeNum := '0'
+
+	// accounting for int 0
+	if nb == 0 {
+		z01.PrintRune(runeNum)
+	}
+
+	for i := 1; i <= nb%10; i++ {
+		runeNum++
+	}
+
+	for i := -1; i >= nb%10; i-- {
+		runeNum++
+	}
+
+	if nb/10 != 0 {
+		Convert(nb / 10)
+	}
+	z01.PrintRune(runeNum)
+}
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	Convert(n)
+}

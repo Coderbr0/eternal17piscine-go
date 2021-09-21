51d8955e953d4066decf8327c64c599ded053a94
eternal17 <sarmadkhatri15@gmail.com>
2021-09-21 10:06:05 +0100

    true if sorted, false otherwise

--- issorted.go
+++ issorted.go
@@ -1,4 +1,20 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	for i := 0; i <= len(a); i++ {
+		if f((a[i]), (a[i+1])) >= 0 || f((a[i]), (a[i+1])) < 0 {
+			return false
+		}
+	}
+	return true
 }
+
+// func f(a, b int) int {
+// 	if a == b {
+// 		return 0
+// 	} else if a > b {
+// 		return 1
+// 	} else {
+// 		return 0
+// 	}
+// }

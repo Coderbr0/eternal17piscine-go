90d73cad3a03c63a1d6e3277dc2f6846538e29e0
eternal17 <sarmadkhatri15@gmail.com>
2021-09-21 10:33:13 +0100

    all true conditions

--- issorted.go
+++ issorted.go
@@ -4,8 +4,10 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i <= len(a); i++ {
 		if f((a[i]), (a[i+1])) == 1 || f((a[i]), (a[i+1])) == -1 || f((a[i]), (a[i+1])) == 0 {
 			return true
+		}
+		return false
 	}
-	return false
+	return true
 }
 
 // func f(a, b int) int {

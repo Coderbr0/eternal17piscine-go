b29ca9baab2849a5d7560158747a934595679f87
eternal17 <sarmadkhatri15@gmail.com>
2021-09-21 17:30:07 +0100

    adding comments

--- strrev.go
+++ strrev.go
@@ -1,9 +1,14 @@
 package piscine
 
 func StrRev(s string) string {
+	// converting to runes
 	runes := []rune(s)
+
+	// iterating from 0 forwards and last character of string backwards
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		// swapping around the characters
 		runes[i], runes[j] = runes[j], runes[i]
 	}
+	// converting the runes back to string as our function needs to return string
 	return string(runes)
 }

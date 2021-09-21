491a40e84b78668a3f9e7f2e92df6ff87b7f46c2
eternal17 <sarmadkhatri15@gmail.com>
2021-09-21 15:17:53 +0100

    printing all combinations of 2, 2 digit numbers

--- printcomb2.go
+++ printcomb2.go
@@ -5,26 +5,23 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for d1 := '0'; d1 <= '9'; d1++ {
 		for d2 := '0'; d2 <= '9'; d2++ {
+			d4 := d2 + 1
 			for d3 := '0'; d3 <= '9'; d3++ {
-				for d4 := '0'; d4 <= '9'; d4++ {
-					if d1 == '9' && d2 == '9' && d3 == '9' && d4 == '9' {
-						z01.PrintRune(d1)
-						z01.PrintRune(d2)
-						z01.PrintRune(' ')
-						z01.PrintRune(d3)
-						z01.PrintRune(d4)
-						z01.PrintRune('\n')
-					} else {
-						z01.PrintRune(d1)
-						z01.PrintRune(d2)
-						z01.PrintRune(' ')
-						z01.PrintRune(d3)
-						z01.PrintRune(d4)
+				for ; d4 <= '9'; d4++ {
+					z01.PrintRune(d1)
+					z01.PrintRune(d2)
+					z01.PrintRune(' ')
+					z01.PrintRune(d3)
+					z01.PrintRune(d4)
+					if d1 < '9' || d2 < '8' || d3 < '9' || d4 < '9' {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
 				}
+				d4 = '0'
 			}
 		}
+
 	}
+	z01.PrintRune('\n')
 }

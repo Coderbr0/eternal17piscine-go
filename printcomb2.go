e11369d59fa5f947eaf4c6a41c06a4ea59c4af0a
eternal17 <sarmadkhatri15@gmail.com>
2021-09-21 15:31:36 +0100

    sorting out conditions

--- printcomb2.go
+++ printcomb2.go
@@ -6,7 +6,7 @@ func PrintComb2() {
 	for d1 := '0'; d1 <= '9'; d1++ {
 		for d2 := '0'; d2 <= '9'; d2++ {
 			d4 := d2 + 1
-			for d3 := '0'; d3 <= '9'; d3++ {
+			for d3 := d1; d3 <= '9'; d3++ {
 				for ; d4 <= '9'; d4++ {
 					z01.PrintRune(d1)
 					z01.PrintRune(d2)

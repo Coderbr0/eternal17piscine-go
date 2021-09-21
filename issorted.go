a2e25616fbb8c2226c921b78d8cd9dd22abaca80
eternal17 <sarmadkhatri15@gmail.com>
2021-09-21 10:14:19 +0100

    changing condition

--- issorted.go
+++ issorted.go
@@ -2,11 +2,11 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i <= len(a); i++ {
-		if f((a[i]), (a[i+1])) >= 0 || f((a[i]), (a[i+1])) < 0 {
-			return false
+		if f((a[i]), (a[i+1])) >= 0 {
+			return true
 		}
 	}
-	return true
+	return false
 }
 
 // func f(a, b int) int {

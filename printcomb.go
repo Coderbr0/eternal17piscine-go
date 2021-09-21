34e2ba17054dcd4c670f7768d99d1adabd5d6b64
eternal17 <sarmadkhatri15@gmail.com>
2021-09-21 14:17:11 +0100

    redone printcomb

--- printcomb.go
+++ printcomb.go
@@ -6,20 +6,19 @@ func PrintComb() {
 	for digit1 := '0'; digit1 <= '7'; digit1++ {
 		for digit2 := '1'; digit2 <= '8'; digit2++ {
 			for digit3 := '2'; digit3 <= '9'; digit3++ {
-				if digit1 < digit2 && digit2 < digit3 {
+				if digit1 == '7' && digit2 == '8' && digit3 == '9' {
+					z01.PrintRune(digit1)
+					z01.PrintRune(digit2)
+					z01.PrintRune(digit3)
+					z01.PrintRune('\n')
+				} else if digit1 < digit2 && digit2 < digit3 {
 					z01.PrintRune(digit1)
 					z01.PrintRune(digit2)
 					z01.PrintRune(digit3)
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
-				if digit1 == '7' && digit2 == '8' && digit3 == '9' {
-					z01.PrintRune(digit1)
-					z01.PrintRune(digit2)
-					z01.PrintRune(digit3)
-				}
 			}
 		}
 	}
-	z01.PrintRune('\n')
 }

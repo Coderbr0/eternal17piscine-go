147b28c8cd1c92d39d20f947fb3a844f4ecd0216
eternal17 <sarmadkhatri15@gmail.com>
2021-09-22 15:28:31 +0100

    changed if statement

--- abort.go
+++ abort.go
@@ -3,11 +3,13 @@ package piscine
 func Abort(a, b, c, d, e int) int {
 	var arr []int
 	arr = append(arr, a, b, c, d, e)
-
-	for i := 0; i < len(arr)-1; i++ {
+	i := 0
+	for i < len(arr)-1 {
 		if arr[i] > arr[i+1] {
 			arr[i], arr[i+1] = arr[i+1], arr[i]
 			i = 0
+		} else {
+			i++
 		}
 	}
 	return arr[2]

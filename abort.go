b537cd6e8bf2bfe24c64d6b5bab20878879c99df
eternal17 <sarmadkhatri15@gmail.com>
2021-09-22 15:04:04 +0100

    appending arr, sorting and returning median

--- abort.go
+++ abort.go
@@ -1,6 +1,14 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	median := (a + b + c + d + e) / 5
-	return median
+	var arr []int
+	arr = append(arr, a, b, c, d, e)
+
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] > arr[i+1] {
+			arr[i], arr[i+1] = arr[i+1], arr[i]
+			i = 0
+		}
+	}
+	return arr[2]
 }

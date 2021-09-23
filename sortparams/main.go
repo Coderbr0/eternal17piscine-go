ee0b9dbbd5267d343b269d7aac6c633841af132e
eternal17 <sarmadkhatri15@gmail.com>
2021-09-23 18:48:46 +0100

    sorting args in terms of ascii values

--- sortparams/main.go
+++ sortparams/main.go
@@ -1,28 +1,3 @@
-// package main
-
-// import (
-// 	"os"
-
-// 	"github.com/01-edu/z01"
-// )
-
-// func main() {
-// 	// convert into args using os
-// 	args := os.Args[1:]
-
-// 	for i := range args {
-// 		ar := []rune(args[i])
-// 		j := 0
-// 		if '0' <= ar[j] && ar[j] <= '9' {
-// 			for _, j := range args[i] {
-// 				z01.PrintRune(j)
-// 				z01.PrintRune('\n')
-// 			}
-// 		}
-
-// 	}
-
-// }
 package main
 
 import (
@@ -32,36 +7,22 @@ import (
 )
 
 func main() {
-	arguments := os.Args[1:]
+	// convert into args using os
+	args := os.Args[1:]
 
-	for i := range arguments {
-		runes := []rune(arguments[i])
-		j := 0
-		if runes[j] >= '0' && runes[j] <= '9' {
-			for _, j := range arguments[i] {
-				z01.PrintRune(j)
-				z01.PrintRune('\n')
-			}
+	i := 0
+	for i < len(args)-1 {
+		if args[i] > args[i+1] {
+			args[i], args[i+1] = args[i+1], args[i]
+			i = 0
+		} else {
+			i++
 		}
 	}
-	for i := range arguments {
-		runes := []rune(arguments[i])
-		j := 0
-		if runes[j] >= 'A' && runes[j] <= 'Z' {
-			for _, j := range arguments[i] {
-				z01.PrintRune(j)
-				z01.PrintRune('\n')
-			}
+	for i := range args {
+		for _, ch := range args[i] {
+			z01.PrintRune(ch)
 		}
-	}
-	for i := range arguments {
-		runes := []rune(arguments[i])
-		j := 0
-		if runes[j] >= 'a' && runes[j] <= 'z' {
-			for _, j := range arguments[i] {
-				z01.PrintRune(j)
-				z01.PrintRune('\n')
-			}
-		}
+		z01.PrintRune('\n')
 	}
 }

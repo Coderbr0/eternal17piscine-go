703eff01e158bdc2651c8587835ecbc5d0322ee1
eternal17 <sarmadkhatri15@gmail.com>
2021-09-23 10:12:27 +0100

    using args

---
+++ comcheck/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args
+
+	for _, arg := range args {
+		if arg == "01" || arg == "galaxy" || arg == "01 galaxy" {
+			fmt.Println("Alert!!!")
+		} else {
+
+		}
+	}
+
+}

7e63c00cc00e10cba05ddb83db1c67a961032f6d
eternal17 <sarmadkhatri15@gmail.com>
2021-09-23 22:48:44 +0100

    redoing

--- printnbr.go
+++ printnbr.go
@@ -2,21 +2,21 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func Convert(nb int) {
-	runeNum := '0'
+func Convert(nbr int) {
+	numRune := '0'
 
-	for i := 1; i <= nb%10; i++ {
-		runeNum++
+	for i := 1; i <= nbr%10; i++ {
+		numRune++
 	}
 
-	for i := -1; i >= nb%10; i-- {
-		runeNum++
+	for i := -1; i >= nbr%10; i-- {
+		numRune++
 	}
 
-	if nb/10 != 0 {
-		Convert(nb / 10)
+	if nbr/10 != 0 {
+		Convert(nbr / 10)
 	}
-	z01.PrintRune(runeNum)
+	z01.PrintRune(numRune)
 }
 
 func PrintNbr(n int) {

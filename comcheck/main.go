e32fe099daee65acf09f1425cc20caca93723e26
eternal17 <sarmadkhatri15@gmail.com>
2021-09-23 10:39:21 +0100

    introduced counter to print alert as many times

--- comcheck/main.go
+++ comcheck/main.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 	args := os.Args
-
+	count := 0
 	for _, arg := range args {
 		if arg == "01" || arg == "galaxy" || arg == "01 galaxy" {
-			fmt.Println("Alert!!!")
+			count++
 		}
+	}
+	for i := 1; i <= count; i++ {
+		fmt.Println("Alert!!!")
+
 	}
 }

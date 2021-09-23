d360d7af6a704b71385520df8031e04ec2826aed
eternal17 <sarmadkhatri15@gmail.com>
2021-09-24 00:22:19 +0100

    testing

--- index.go
+++ index.go
@@ -3,7 +3,7 @@ package piscine
 func Index(s string, toFind string) int {
 	var firstIndex int
 
-	for i := range s {
+	for i := 0; i < len(s); i++ {
 		if s[i] == toFind[0] {
 			firstIndex = i
 			temp := s[i : i+len(toFind)]

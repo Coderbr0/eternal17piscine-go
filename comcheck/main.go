6589b3d5e1f9e828000fa84d3898558d625de78a
eternal17 <sarmadkhatri15@gmail.com>
2021-09-23 10:44:17 +0100

    changed

--- comcheck/main.go
+++ comcheck/main.go
@@ -7,13 +7,14 @@ import (
 
 func main() {
 	args := os.Args
-	count := 0
+	// count := 0
 	for _, arg := range args {
 		if arg == "01" || arg == "galaxy" || arg == "01 galaxy" {
-			count++
+			// count++
+			fmt.Println("Alert!!!")
 		}
 	}
-	for i := 1; i <= count; i++ {
-		fmt.Println("Alert!!!")
-	}
+	// for i := 1; i <= count; i++ {
+	// 	fmt.Println("Alert!!!")
+	// }
 }

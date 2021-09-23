731145d65d7cb51c54df9249504bb75537b4b52e
eternal17 <sarmadkhatri15@gmail.com>
2021-09-23 19:49:06 +0100

    converting runes to numbers and then correspoding alpha

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	caps := false
+
+	for i := range args {
+		if args[i] == "--upper" {
+			caps = true
+		}
+	}
+	for _, arg := range args {
+		num := 0
+		for _, n := range arg {
+			num = num*10 + int(n-48)
+		}
+		if 1 <= num && num <= 26 {
+			if caps == false {
+				z01.PrintRune(rune(num + 96))
+			} else {
+				z01.PrintRune(rune(num + 64))
+			}
+		} else {
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune('\n')
+}

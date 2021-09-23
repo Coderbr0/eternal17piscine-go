14732c10b6c6681a3e8596c561cb09d1945039b3
eternal17 <sarmadkhatri15@gmail.com>
2021-09-23 11:38:35 +0100

    condition to only print if count is greater or equal to 1

--- comcheck/main.go
+++ comcheck/main.go
@@ -7,14 +7,13 @@ import (
 
 func main() {
 	args := os.Args
-	// count := 0
+	count := 0
 	for _, arg := range args {
 		if arg == "01" || arg == "galaxy" || arg == "01 galaxy" {
-			// count++
-			fmt.Println("Alert!!!")
+			count++
 		}
 	}
-	// for i := 1; i <= count; i++ {
-	// 	fmt.Println("Alert!!!")
-	// }
+	if count >= 1 {
+		fmt.Println("Alert!!!")
+	}
 }
